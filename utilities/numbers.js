31d42d87050fd3b43f58f603ba789f66fe4be50f
Hugh's Coding Adventure <50379908+hughmikem22@users.noreply.github.com>
2022-04-19 18:23:14 -0500

    Add files via upload
    All required files are uploaded save the node modules folder (due to size) that is created when you run npm init

---
+++ utilities/numbers.js
@@ -0,0 +1,27 @@
+//multiply
+const multiply = (num1, num2) => {
+    return num1 * num2;
+};
+//add
+const sum = (num1, num2) => {
+    return num1 + num2;
+};
+//subtract
+const subtract = (num1, num2) => {
+    return num1 - num2;
+};
+// multiply
+const divide = (num1, num2) => {
+    return num1 / num2;
+};
+// sqaure
+const square = (num) => {
+    return num * num;
+};
+export default {
+    multiply,
+    subtract,
+    divide,
+    square,
+    sum
+};
---
+++ utilities/numbers.ts
@@ -0,0 +1,35 @@
+//multiply
+
+const multiply = (num1: number, num2: number): number => {
+    return num1 * num2;
+};
+
+//add
+const sum = (num1: number, num2: number): number =>{
+    return num1 + num2;
+};
+
+//subtract
+const subtract = (num1: number, num2:number): number =>{
+    return num1 - num2;
+
+};
+
+// multiply
+const divide = (num1: number, num2:number): number =>{
+    return  num1 / num2;
+};
+
+// sqaure
+const square = (num: number): number => {
+    return num * num;
+};
+
+export default {
+    multiply,
+    subtract,
+    divide,
+    square,
+    sum
+};
+
